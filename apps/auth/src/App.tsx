57c611965a636be3503f704c89f6a941558b0aa4
Loïk Fekkaï <lokicoule@yahoo.fr>
2022-11-16 09:44:15 +0100

    feat(auth): mfe setup

--- apps/auth/src/App.tsx
+++ apps/auth/src/App.tsx
@@ -1,25 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { AppShell } from "ui";
+import HomeIcon from "@mui/icons-material/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppShell
+      title="Playlist"
+      colorScheme="dark"
+      routes={[
+        {
+          path: "/",
+          element: () => <div>Home</div>,
+        },
+        {
+          path: "/playlist",
+          element: () => <div>Playlist</div>,
+        },
+      ]}
+      navLinks={[
+        {
+          label: "Home",
+          path: "/",
+          icon: <HomeIcon />,
+        },
+      ]}
+    />
   );
 }
 
