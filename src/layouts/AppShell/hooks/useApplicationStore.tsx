7a6f293aef092ce38ec38a47f01e5c02f45cd321
Loïk Fekkaï <lokicoule@yahoo.fr>
2022-12-02 15:18:44 +0100

    feat(): update palette

--- src/layouts/AppShell/hooks/useApplicationStore.tsx
+++ src/layouts/AppShell/hooks/useApplicationStore.tsx
@@ -14,6 +14,18 @@ export const useApplicationStore = () => {
       createTheme({
         palette: {
           mode: mode ?? "light",
+          primary: {
+            light: "#33ab9f",
+            main: "#009688",
+            dark: "#00695f",
+            contrastText: "#fff",
+          },
+          secondary: {
+            light: "#f73378",
+            main: "#f50057",
+            dark: "#ab003c",
+            contrastText: "#000",
+          },
         },
       }),
     [mode]
