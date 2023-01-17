ef3efabe65d3396eedbfec17836d347d4ac967eb
Loïk Fekkaï <lokicoule@yahoo.fr>
2023-01-17 21:34:33 +0100

    feat(mode): toggle button

--- src/components/Settings/Mode.tsx
+++ src/components/Settings/Mode.tsx
@@ -1,41 +1,46 @@
 import DarkIcon from "@mui/icons-material/DarkModeOutlined";
 import LightIcon from "@mui/icons-material/LightModeOutlined";
 import SystemIcon from "@mui/icons-material/SettingsBrightnessOutlined";
-import { Button, ButtonGroup } from "@mui/material";
+import {
+  Button,
+  ButtonGroup,
+  Stack,
+  ToggleButton,
+  ToggleButtonGroup,
+  Typography,
+} from "@mui/material";
 import { useTheme } from "~/providers/theme";
 
 export const Mode = () => {
   const { light, dark, system, mode } = useTheme();
 
   return (
-    <ButtonGroup
+    <ToggleButtonGroup
+      color="primary"
+      value={mode}
+      exclusive
       sx={{
-        width: "100%",
         justifyContent: "center",
       }}
-      variant="outlined"
     >
-      <Button
-        startIcon={<LightIcon />}
-        onClick={light}
-        variant={mode === "light" ? "contained" : "outlined"}
-      >
-        Light
-      </Button>
-      <Button
-        startIcon={<SystemIcon />}
-        onClick={system}
-        variant={mode === "system" ? "contained" : "outlined"}
-      >
-        System
-      </Button>
-      <Button
-        startIcon={<DarkIcon />}
-        onClick={dark}
-        variant={mode === "dark" ? "contained" : "outlined"}
-      >
-        Dark
-      </Button>
-    </ButtonGroup>
+      <ToggleButton onClick={light} value={"light"}>
+        <Stack direction="row" spacing={1}>
+          <LightIcon />
+          <Typography>Light</Typography>
+        </Stack>
+      </ToggleButton>
+      <ToggleButton onClick={system} value="system">
+        <Stack direction="row" spacing={1}>
+          <SystemIcon />
+          <Typography>System</Typography>
+        </Stack>
+      </ToggleButton>
+      <ToggleButton onClick={dark} value="dark">
+        <Stack direction="row" spacing={1}>
+          <DarkIcon />
+          <Typography>Dark</Typography>
+        </Stack>
+      </ToggleButton>
+    </ToggleButtonGroup>
   );
 };
